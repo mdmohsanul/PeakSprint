08a26be6c8b3f41fe4d077f28478e7d0b72a5dbb
Mohsanul786 <mdmohsan2407@gmail.com>
2025-10-27 11:05:19 +0530

    evil-commit

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
 export const Button = (props: ButtonProps) => {
   const { onClick } = props;
   const handler = () => {
-    console.log("handler");
+    console.log("handler button");
   };
   if (!onClick)
     return <button onClick={handler} data-testid="button-test"></button>;
