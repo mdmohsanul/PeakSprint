1feb77e7020c2e4178246d5fbcd12e1313e01e7c
Mohsanul786 <mdmohsan2407@gmail.com>
2025-10-27 11:30:50 +0530

    add button name

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,9 +6,13 @@ interface ButtonProps{
 }
 export const Button = (props:ButtonProps) => {
     const {onClick} = props
-  return <>
-     <button onClick={onClick} data-testid="button-test"></button>
-  </>
+  return (
+    <>
+      <button onClick={onClick} data-testid="button-test">
+        Click Me
+      </button>
+    </>
+  );
 };
 
 
